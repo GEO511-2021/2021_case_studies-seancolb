64c4f46346a61de2a06059c38b8c4a65da0695d1
Sean Colby <seancolb@buffalo.edu>
2021-09-21 10:32:53 -0400

    first commit

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,12 @@
- 
+#install packages
+install.packages("gapminder")
+
+#load libraries 
+library(ggplot2)
+library(gapminder)
+library(dplyr)
+
+data(gapminder)
+
+gapminder%>%
+  filter(country != "kuwait")
