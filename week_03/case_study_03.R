739dc19328791f3ff4ebb9559e51ebc54a3cbd21
Sean Colby <seancolb@buffalo.edu>
2021-09-21 10:54:11 -0400

    second commit

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -6,7 +6,17 @@ library(ggplot2)
 library(gapminder)
 library(dplyr)
 
+#load as data
 data(gapminder)
+str(gapminder)
 
-gapminder%>%
+#Get rid of kuwait
+Countries_No_Kuwait <-gapminder%>%
   filter(country != "kuwait")
+
+#change pop
+gapminder%>% mutate(pop= pop/100000)
+
+#plot
+ggplot(Countries_No_Kuwait, aes(x= lifeExp, y=gdpPercap, color= continent, size=pop))+
+  geom_point()
