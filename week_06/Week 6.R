617744f6993b9737679a4e024a3bfb4caf075298
Sean Colby <seancolb@buffalo.edu>
2021-10-12 10:30:09 -0400

    first commit

---
+++ week_06/Week 6.R
@@ -0,0 +1,36 @@
+#Load in Libraries
+library(raster)
+library(sp)
+library(spData)
+library(tidyverse)
+library(sf)
+
+#Load Data in
+data(world)
+tmax_monthly <- getData(name= "worldclim", var="tmax", res=10)
+
+#Peek at world data
+str(world)
+
+#Remove Antarctica 
+Countries <- filter(world, continent != "Antarctica")
+
+#Set as sp
+Countries_sp <- as(Countries, "Spatial")
+
+#Peek at Temperature
+str(tmax_monthly, max.level=2)
+tmax_monthly
+
+#What does gain do?
+?gain()
+
+#Convert Temperature Value
+gain(tmax_monthly)= .1
+
+#Find max annual
+tmax_annual <- max(tmax_monthly)
+tmax <- names(tmax_annual)
+
+#Calculate and plot maximum temperature per country
+
