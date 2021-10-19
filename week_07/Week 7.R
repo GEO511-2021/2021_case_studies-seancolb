597b1f2fe6e9026f423902b89de1d015ccfb633a
Sean Colby <seancolb@buffalo.edu>
2021-10-19 10:19:31 -0400

    Reprex Finally Worked

--- week_07/Week 7.R
+++ week_07/Week 7.R
@@ -1,11 +1,15 @@
+#Load Library
 library(tidyverse)
 library(reprex)
 library(sf)
 library(spData)
 
+#Load Data
 data(world)
 
+#Plot it
 ggplot(world,aes(x=gdpPercap, fill=continent, color=continent))+
   geom_density(alpha=0.5)
 
+#Reprex
 reprex(venue="gh")
