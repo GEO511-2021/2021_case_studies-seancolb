9bd99f636ac7082c98a9ce5c7b48186f8ba7e9a8
Sean Colby <seancolb@buffalo.edu>
2021-10-19 10:11:37 -0400

    Fixed Code

--- week_07/Week 7.R
+++ week_07/Week 7.R
@@ -1,14 +1,11 @@
-#Load Libraries in
 library(tidyverse)
 library(reprex)
 library(sf)
 library(spData)
 
-#Load Data in 
 data(world)
 
-#Generate Figure
-ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
-  geom_density(alpha=0.5,color=F)
+ggplot(world,aes(x=gdpPercap, fill=continent, color=continent))+
+  geom_density(alpha=0.5)
 
 reprex(venue="gh")
