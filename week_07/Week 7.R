2d8fdfca442eea80180646ddb247052ee36448f0
Sean Colby <seancolb@buffalo.edu>
2021-10-19 09:56:51 -0400

    Initial Commit with error

---
+++ week_07/Week 7.R
@@ -0,0 +1,14 @@
+#Load Libraries in
+library(tidyverse)
+library(reprex)
+library(sf)
+library(spData)
+
+#Load Data in 
+data(world)
+
+#Generate Figure
+ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
+  geom_density(alpha=0.5,color=F)
+
+
